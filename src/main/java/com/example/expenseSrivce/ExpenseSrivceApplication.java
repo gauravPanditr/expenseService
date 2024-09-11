366ac6481465ef111d6e813b9d5ef9ef47ff9f02
gauravPanditr <gauravpandit7856@gmail.com>
2024-09-11 16:29:04 +0530

    creating expenseService

---
+++ src/main/java/com/example/expenseSrivce/ExpenseSrivceApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenseSrivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSrivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSrivceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expenseSrivce/ExpenseSrivceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenseSrivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSrivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
